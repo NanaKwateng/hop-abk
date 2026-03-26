6f18b35dd7dc0ad2d22323cbcc1813209b749d65
NanaKwateng <nanakwateng172@gmail.com>
2026-03-26 13:31:22 +0000

    supressed hydration warning

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }) {
     return (
-        <html lang="en">
+        <html lang="en" suppressHydrationWarning>
             <body className={`${spaceGrotesk.variable} ${playfair.variable} ${inter.variable} font-sans bg-black text-white antialiased`}>
                 {children}
             </body>
