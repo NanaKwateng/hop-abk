44da12a71c695bdf3727077cd2da23b640ad12e2
NanaKwateng <nanakwateng172@gmail.com>
2026-04-01 04:47:45 +0000

    fixed manifeest file

--- app/layout.tsx
+++ app/layout.tsx
@@ -74,7 +74,7 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="HOP ABK" />
-
+        <link rel="manifest" href="/manifest.json" />
         {/* ── Android / General ── */}
         <meta name="mobile-web-app-capable" content="yes" />
 
